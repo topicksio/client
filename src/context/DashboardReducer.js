f82d29ad0fa793230945771921ed8a290f1784ee
jmendez44 <jenuel.mendez@gmail.com>
2020-04-14 20:11:20 -0400

    reducer working

--- src/context/DashboardReducer.js
+++ src/context/DashboardReducer.js
@@ -1,7 +1,7 @@
 export default (state, action) => {
-  const { from, topic, topic_folder } = action.payload;
+  const { topic_folder, id } = action.payload;
   switch (action.type) {
-    case "RECIEVE_TOPIC":
+    case "DELETE_TOPIC":
       return {
         ...state,
         [topic_folder]: [...state[topic_folder], { from, topic }],
--- src/context/DashboardStore.js
+++ src/context/DashboardStore.js
@@ -2,7 +2,6 @@ import React, { createContext, useReducer } from "react";
 import DashboardReducer from "./DashboardReducer";
 import io from "socket.io-client";
 
-
 /*
   topic:
   {
@@ -24,20 +23,16 @@ import io from "socket.io-client";
 
 const initState = {
   topic_folder1: [
-    { from: "jayjay", topic: "covid19" },
-    { from: "jean", topic: "dbz" },
-    { from: "shay", topic: "the moon" },
+    { from: "jayjay", topic: "covid19", id: 1 },
+    { from: "jean", topic: "dbz", id: 2 },
+    { from: "shay", topic: "the moon", id: 3 },
   ],
   topic_folder2: [
-    { from: "jack", topic: "of all" },
-    { from: "nolienol", topic: "idk man" },
-    { from: "paul", topic: "hardware" },
+    { from: "jack", topic: "of all", id: 1 },
+    { from: "nolienol", topic: "idk man", id: 2 },
+    { from: "paul", topic: "hardware", id: 3 },
   ],
 };
-
-
-
-
 
 // Create context
 export const GlobalContext = createContext(initState);
@@ -45,7 +40,16 @@ export const GlobalContext = createContext(initState);
 // Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(DashboardReducer, initState);
-  
+
+  //ACTIONS
+
+  function deleteTopic( topic_folder, id) {
+    dispatch({
+      type: 'DELETE_TOPIC',
+      payload: topic_folder, id
+    })
+  }
+
   return (
     <GlobalContext.Provider value={{ topics: state }}>
       {children}
