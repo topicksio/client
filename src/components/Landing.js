7eb441b076b6bc32ff9f23d9c81a4f6c84bd136d
jmendez44 <jenuel.mendez@gmail.com>
2020-07-07 05:14:33 -0400

    landing page design

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logoBLACK.png";
 
 export const Landing = () => {
   return (
@@ -7,15 +7,23 @@ export const Landing = () => {
       <nav>
         <img src={Logo} alt="" />
         <ul>
-          <li>Home</li>
-          <li>About</li>
           <li>
-            <a href='https://id.twitch.tv/oauth2/authorize?client_id=mpjn8uocr7w1yt5b34ihtllgzlxfu1&amp;redirect_uri=http://localhost:3000/callback&amp;response_type=id_token&amp;scope=openid+user:read:email&amp;claims={"id_token":{"email":null,"email_verified":null,"picture":null, "preferred_username":null}}'>
-              login with twitch
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Support</a>
+          </li>
+          <li>
+            <a
+              className="twitch"
+              href='https://id.twitch.tv/oauth2/authorize?client_id=mpjn8uocr7w1yt5b34ihtllgzlxfu1&amp;redirect_uri=http://localhost:3000/callback&amp;response_type=id_token&amp;scope=openid+user:read:email&amp;claims={"id_token":{"email":null,"email_verified":null,"picture":null, "preferred_username":null}}'
+            >
+              Login With Twitch
             </a>
           </li>
         </ul>
       </nav>
+      <div className="main-landing"></div>
     </div>
   );
 };
