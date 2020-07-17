dedcb98cb0b79333b1964fd5a7a6f9c46c5c304c
jmendez44 <jenuel.mendez@gmail.com>
2020-07-16 23:41:08 -0400

    --

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,7 +18,7 @@ export const Landing = () => {
               className="twitch"
               href='https://id.twitch.tv/oauth2/authorize?client_id=mpjn8uocr7w1yt5b34ihtllgzlxfu1&amp;redirect_uri=http://localhost:3000/callback&amp;response_type=id_token&amp;scope=openid+user:read:email&amp;claims={"id_token":{"email":null,"email_verified":null,"picture":null, "preferred_username":null}}'
             >
-              Login With Twitch
+              Login With Twitch!
             </a>
           </li>
         </ul>
