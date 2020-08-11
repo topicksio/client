59272b00b8b5494c896f8c5dacf66427bd879a44
jmendez44 <jenuel.mendez@gmail.com>
2020-08-10 22:40:01 -0400

    basic landing page css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -25,22 +25,18 @@ export const Landing = () => {
         </ul>
       </nav>
       <div className="main-landing">
-        <motion.div
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 270, 270, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-          }}
-          transition={{
-            duration: 2,
-            ease: "easeInOut",
-            times: [0, 0.2, 0.5, 0.8, 1],
-            loop: Infinity,
-            repeatDelay: 1,
-          }}
-        >
-          <div>yo</div>
-        </motion.div>
+        <div className="landing-left"></div>
+        <div className="landing-right">
+          <motion.div className="mock-card">
+            <div>yo</div>
+          </motion.div>
+          <motion.div className="mock-card">
+            <div>yo</div>
+          </motion.div>
+          <motion.div className="mock-card">
+            <div>yo</div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
